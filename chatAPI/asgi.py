55f3129c53f649bc5968d76ff897ed15b4b6ebd9
David Duyile <femiduyile@gmail.com>
2025-03-06 23:11:29 +0100

    Update asgi.py

--- chatAPI/asgi.py
+++ chatAPI/asgi.py
@@ -3,7 +3,6 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from chat.routing import websocket_urlpatterns 
 from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
-from channels.security.websocket import AllowedHostsOriginValidator
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatAPI.settings')
 
@@ -11,7 +10,5 @@ http_response_app = get_asgi_application()
 
 application =ProtocolTypeRouter({
     "http": http_response_app,
-    "websocket": AllowedHostsOriginValidator(
-        AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
-    )
+    "websocket": AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
 })
